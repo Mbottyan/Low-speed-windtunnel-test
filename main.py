7542001f8b598aeebbd7c03a19db30a66abcc5d3
Dmytro <gldima1208@gmail.com>
2025-12-10 12:06:45 +0100

    moment

--- main.py
+++ main.py
@@ -36,12 +36,38 @@ def lift_drag_surface_alpha(alpha):
     drag=normal_force(c,location_top, location_bottom, pressure_top, pessure_bottom)*sin(alpha)+tangential_force(location_y_top,location_y_bottom, pressure_top, pessure_bottom)*cos(alpha)
     return lift, drag
 
-lift_drag_wake_alpha(alpha):
+def lift_drag_wake_alpha(alpha):
     normal=normal_force(c,location_top, location_bottom, pressure_top, pessure_bottom)
     tangential=(drag_velocity(u_inf,u_y,p_inf, p_y,rho, y_locations)-norma*sin(alpha))/cos(alpha)
     lift=normal*cos(alpha)-tangential*sin(alpha)
-    drag=+normal*sin(alpha)+tangentiatangential*cos(alpha)
+    drag=+normal*sin(alpha)+tangential*cos(alpha)
     return lift,drag
+
+def moment(c,location_top, location_bottom,location_y_top,location_y_bottom, pressure_top, pessure_bottom):
+    diff_top = np.array([location_top[i] - location_top[i-1] for i in range(1, len(location_top))])
+    diff_bottom = np.array([location_bottom[i] - location_bottom[i-1] for i in range(1, len(location_bottom))])
+    diff_y_top = np.array([location_y_top[i] - location_y_top[i-1] for i in range(1, len(location_y_top))])
+    diff_y_bottom= np.array([location_y_bottom[i] - location_y_bottom[i-1] for i in range(1, len(location_y_bottom))])
+
+    avg_x_top= np.array([(location_top[i] + location_top[i-1])/2 for i in range(1, len(location_top))])
+    avg_x_bottom = np.array([(location_bottom[i] + location_bottom[i-1])/2 for i in range(1, len(location_bottom))])
+    avg_x_y_top = np.array([(location_y_top[i] + location_y_top[i-1])/2 for i in range(1, len(location_y_top))])
+    avg_x_y_bottom= np.array([(location_y_bottom[i] + location_y_bottom[i-1])/2 for i in range(1, len(location_y_bottom))])
+
+    avg_pressure_top=np.array([(pressure_top[i] + pressure_top[i-1])/2 for i in range(1, len(pressure_top))])
+    avg_pressure_bottom=np.array([(pessure_bottom[i] + pessure_bottom[i-1])/2 for i in range(1, len(pessure_bottom))])
+
+    M=0
+    for length, location, pressure  in zip(diff_top,avg_x_top,avg_pressure_top):
+        M+=length*pressure*location
+    for length, location, pressure  in zip(diff_bottom,avg_x_bottom,avg_pressure_bottom):
+        M-=length*pressure*location
+    for length, location, pressure  in zip(diff_y_top,avg_x_y_top,avg_pressure_top):
+        M+=length*pressure*location
+    for length, location, pressure  in zip(diff_y_bottom,avg_x_y_bottom,avg_pressure_bottom):
+        M-=length*pressure*location
+    return M
+
     
     
 
